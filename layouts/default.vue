12b1d47f031a3de97284efd360106063e21a9ee0
km1y <kmly267@gmail.com>
2021-09-01 00:18:58 +0900

    fix: font style

--- layouts/default.vue
+++ layouts/default.vue
@@ -46,7 +46,15 @@ export default {
 }
 
 html {
-  font-family: inherit;
+  font-family:
+    'Source Sans Pro',
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif;
   font-size: 16px;
   word-spacing: 1px;
   -ms-text-size-adjust: 100%;
