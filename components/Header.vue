ef9776d2999b3c5bb4fa74f2a6251b9c6069ddf3
kmly267 <kmly267@gmail.com>
2021-12-29 00:48:19 +0900

    fix: nav position

--- components/Header.vue
+++ components/Header.vue
@@ -1,11 +1,11 @@
 <template>
 <header class="text-gray-400 bg-gray-900 body-font">
   <div class="origin-container flex flex-wrap p-5 flex-col md:flex-row items-center">
-    <nuxt-link to="/" class="flex title-font font-medium items-center text-white mb-4 md:mb-0">
+    <nuxt-link to="/" class="flex title-font font-medium items-center text-white mb-4 mb-0">
       <span class="ml-3 text-xl">kmly267.github.io</span>
     </nuxt-link>
     <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
-      <nuxt-link to="/" class="mr-5 hover:text-white">Home</nuxt-link>
+      <nuxt-link to="/" class="mr-5 hover:text-white ml-5">Home</nuxt-link>
       <nuxt-link to="/posts/" class="mr-5 hover:text-white">Blog</nuxt-link>
     </nav>
   </div>
