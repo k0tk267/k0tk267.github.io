b0fcd0cf64b41077d5ba333d1f83d19692f9db3b
kmly267 <kmly267@gmail.com>
2021-12-29 00:50:10 +0900

    build: generate

--- dist/_nuxt/static/1631806482/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fposts","\u002Fposts\u002Fnaist-entrance-exam-2021","\u002Fposts\u002Fcyber-agent-web-speed-hackathon-2021","\u002Fposts\u002Fresolution-in-2021","\u002Fposts\u002Freview-in-2020"]})
