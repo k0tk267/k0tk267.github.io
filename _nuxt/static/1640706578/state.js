ec57099e72246d9b7dd24dcb6af264f4e96d4d5a
k0tk267 <kmly267@gmail.com>
2022-03-29 14:05:25 +0000

    Deploying to gh-pages from  @ 28de825134d74074622f55319939778d0933ed19 🚀

--- _nuxt/static/1640706578/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1640706578",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"c12b197f"}}}}(null,"\u002F"));
