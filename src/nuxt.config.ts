85b2f47b6335dda63521dc8189bae4b99c7b86d6
k0tk267 <k0tk2670@gmail.com>
2022-08-12 19:28:22 +0900

    fix:

--- src/nuxt.config.ts
+++ src/nuxt.config.ts
@@ -2,7 +2,11 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  components: true,
   modules: [
-    "@nuxt/content"
+    '@nuxt/content'
   ],
+  css: [
+    "@/assets/css/awsm_theme_mischka.custom.css"
+  ]
 })
