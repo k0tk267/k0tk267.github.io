d4ef994a04e4966a01038e5866a602b1c5d1fd69
k0tk267 <k0tk2670@gmail.com>
2022-08-12 19:30:18 +0900

    feat: add blog pages

---
+++ src/pages/posts/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <main>
+    <ContentList path="/posts" v-slot="{ list }">
+      <div v-for="article in list" :key="article._path">
+        <NuxtLink :to="article._path">
+          <h2>{{ article.title }}</h2>
+        </NuxtLink>
+        <p>{{ article.date }}</p>
+      </div>
+    </ContentList>
+  </main>
+</template>
