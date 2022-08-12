6ef29394ab07619f1bb47bae15f152e16c74ee5a
k0tk267 <k0tk2670@gmail.com>
2022-08-12 23:02:46 +0900

    fix: blog index

--- src/pages/posts/index.vue
+++ src/pages/posts/index.vue
@@ -1,11 +1,12 @@
 <template>
   <main>
+    <h4>(2022/08/12) <br> 記事以降作業中に付き、画像が表示されない状態となっています。</h4>
     <ContentList path="/posts" v-slot="{ list }">
       <div v-for="article in list" :key="article._path">
         <NuxtLink :to="article._path">
-          <h2>{{ article.title }}</h2>
+          <h3>{{ article.title }}</h3>
         </NuxtLink>
-        <p>{{ article.date }}</p>
+        <span>{{ String(article.date).substr(0, 10) }}</span>
       </div>
     </ContentList>
   </main>
